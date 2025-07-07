a5907b3784b07251a2d546eac78c0d59fa415ecc
Mkamono <chengyaye31+github@gmail.com>
2025-07-07 15:26:17 +0900

    feat: Add OpenAI compatible model support and update web server startup command

--- core/services/aimodel.ts
+++ core/services/aimodel.ts
@@ -1,4 +1,5 @@
 import { google } from "@ai-sdk/google";
+import { createOpenAICompatible } from "@ai-sdk/openai-compatible";
 import { LanguageModel } from "ai";
 
 function getGoogleAiModel(): LanguageModel {
@@ -9,7 +10,18 @@ function getGoogleAiModel(): LanguageModel {
     return google('models/gemini-2.5-flash');
 }
 
+function getOpenaiCompatibleModel(): LanguageModel {
+    const provider = createOpenAICompatible({
+        name: 'OpenAI-Compatible',
+        apiKey: process.env.OPENAI_COMPATIBLE_API_KEY,
+        baseURL: process.env.OPENAI_COMPATIBLE_BASE_URL || '',
+    });
+
+    return provider(process.env.OPENAI_COMPATIBLE_MODEL || 'gpt-3.5-turbo');
+}
+
 export function getAiModel(): LanguageModel {
-    const model = getGoogleAiModel();
+    // const model = getGoogleAiModel();
+    const model = getOpenaiCompatibleModel();
     return model;
 }
