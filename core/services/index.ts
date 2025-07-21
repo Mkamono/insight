48ca8b3b563e80218ff0abc813398f7e9a5b817c
Mkamono <chengyaye31+github@gmail.com>
2025-07-21 10:17:57 +0900

    delete

--- core/services/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './fragmentService.js';
-export * from './documentService.js';
-export * from './tagService.js';
-export * from './aiService.js';
-export * from './resetService.js';
-export * from './questionService.js';
